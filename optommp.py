d59ec54b6e7c639716d1bf90f6ac165124c8e6d0
Andrew Rodgers <acedrew@gmail.com>
2016-02-14 20:55:21 +0000

    added docstrings

--- optommp.py
+++ optommp.py
@@ -1,17 +1,24 @@
 import struct
 
 
-def request_header(tl, tcode):
+def request_header(tcode, tl=16):
+    """Return packed request header.
+
+    Given tcode and tl(transaction label) returns 4 Byte request header
+    common across request messages
+    """
     # universal request header, 4 Bytes long
     return struct.pack('>2xBB', (tl << 2), (tcode << 4))
 
 
 def pack_quadlet_read_request(start_address, tl=16):
+    """Returns packed quadlet read request"""
     return struct.pack('>4s2xHI', request_header(tl, 4),
                        (start_address >> 32), (start_address & (2 ** 32 - 1)))
 
 
 def pack_block_read_request(start_address, data_length, tl=16):
+    """Returns packed block read request"""
     # This packs the arguments into the message format specified
     # by Opto22 Form 1465 "OptoMMP Protocol Guide"
     return struct.pack('>4s2xHIH2x', request_header(tl, 5),
@@ -20,6 +27,7 @@ def pack_block_read_request(start_address, data_length, tl=16):
 
 
 def verify_response_header(response, tcode, tl=16):
+    """Verifies response header, and returns remainder of response"""
     # First we unpack the the header data, and verify there were no errors and
     # the TL code is correct.
     r_tl, r_tcode, r_rcode = struct.unpack('>2x2B2xBx', response[:8])
@@ -30,11 +38,13 @@ def verify_response_header(response, tcode, tl=16):
 
 
 def unpack_quadlet_read_response(response, tl=16):
+    """Returns UINT from quadlet response"""
     verified = verify_response_header(response, 7, tl)
     return struct.unpack('>4xI', verified)
 
 
 def unpack_block_read_response(response, tl=16):
+    """returns tuple of 32b UINTs from block response"""
     verified = verify_response_header(response, 7, tl)
     length, = struct.unpack('>4xH2x', verified[:8])
     # now we unpack the rest of the data, this assumes 32bit uints. This
